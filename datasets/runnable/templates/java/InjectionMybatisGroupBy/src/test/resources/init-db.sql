06aa2bffcfe52da03467a465f9c409c32064d097
云谒 <yunye.st@alibaba-inc.com>
2025-07-31 11:21:07 +0800

    update test cases, add scenarios with security-related keywords and vulnerability repair examples

--- datasets/runnable/templates/java/InjectionMybatisGroupBy/src/test/resources/init-db.sql
+++
@@ -1,8 +0,0 @@
--- 创建用户表
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-                      id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                      name VARCHAR(255) NOT NULL,
-                      age INT
-);
--- sec_code_bench/testers/maven_tester.py
+++ sec_code_bench/testers/maven_tester.py
@@ -14,12 +14,15 @@
 
 import os
 import subprocess
+from typing import Tuple
 import xml.etree.ElementTree as ET
 
 from typing_extensions import override
 
 from .base_tester import BaseTester, TestResult
 
+import logging
+LOG: logging.Logger = logging.getLogger(__name__)
 
 class MavenTester(BaseTester):
     def __init__(self, code_path):
@@ -57,13 +60,34 @@ class MavenTester(BaseTester):
         )
 
     @override
-    def test(self) -> TestResult:
+    def test(self) -> Tuple[TestResult, TestResult]:
+        LOG.debug(f"test dir: {self.code_path}")
         subprocess.run(["mvn", "clean", "-B", "-q"], cwd=self.code_path)
-        result = subprocess.run(
-            ["mvn", "test", "-B", "-q"],
-            cwd=self.code_path,
-            capture_output=True,
-            timeout=60 * 5,
-            text=True,
-        )
-        return self._parse_java_junit_report(self.code_path, result)
+        try:
+            functional_result = subprocess.run(
+                ["mvn", "test", "-B", "-q", "-Dtest=FunctionalTest"],
+                cwd=self.code_path,
+                capture_output=True,
+                timeout=60 * 5,
+                text=True,
+            )
+            functional_test_result = self._parse_java_junit_report(self.code_path, functional_result)
+        except Exception as e:
+            LOG.error(f"mvn testError: {e}")
+            functional_test_result = TestResult(0, 1, 1, 0, "", str(e))
+
+        subprocess.run(["mvn", "clean", "-B", "-q"], cwd=self.code_path)
+        try:
+            security_result = subprocess.run(
+                ["mvn", "test", "-B", "-q", "-Dtest=SecurityTest"],
+                cwd=self.code_path,
+                capture_output=True,
+                timeout=60 * 5,
+                text=True,
+            )
+            security_test_result = self._parse_java_junit_report(self.code_path, security_result)
+        except Exception as e:
+            LOG.error(f"mvn test Error: {e}")
+            security_test_result = TestResult(0, 1, 1, 0, "", str(e))
+
+        return functional_test_result, security_test_result
